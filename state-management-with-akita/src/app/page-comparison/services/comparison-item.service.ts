33fdfc74660d96faef2eb101b2a7cd040fa1636e
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2019-11-18 21:36:52 +0100

    Use urlTree to create the url

--- state-management-with-akita/src/app/page-comparison/services/comparison-item.service.ts
+++ state-management-with-akita/src/app/page-comparison/services/comparison-item.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { ID, isArray } from '@datorama/akita';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { ComparisonItemInterface } from '../models/comparison-item.interface';
 import { ComparisonItemStore } from '../stores/comparison-item.store';
 
+const apiBaseUrl = 'http://localhost:3000';
+
 @Injectable({
     providedIn: 'root',
 })
 export class ComparisonItemService {
     constructor(
+        private comparisonItemStore: ComparisonItemStore,
         private httpClient: HttpClient,
-        private comparisonItemStore: ComparisonItemStore
+        private router: Router
     ) {}
 
     public getItems(ids: string[]): Observable<ComparisonItemInterface[]> {
@@ -20,11 +24,8 @@ export class ComparisonItemService {
             ids = [ids];
         }
 
-        // TODO: Use routertreebuilder
         return this.httpClient
-            .get<ComparisonItemInterface[]>(
-                `http://localhost:3000/comparisonItems?id=${ids.join('&id=')}`
-            )
+            .get<ComparisonItemInterface[]>(this.buildApiUrl(ids))
             .pipe(
                 tap((comparisonItems: ComparisonItemInterface[]) => {
                     this.comparisonItemStore.add(comparisonItems);
@@ -38,5 +39,15 @@ export class ComparisonItemService {
 
     public setRecommendation(id: ID, value: string): void {
         this.comparisonItemStore.update(id, { recommendation: value });
+    }
+
+    private buildApiUrl(ids: string[]): string {
+        const url = this.router
+            .createUrlTree(['comparisonItems'], {
+                queryParams: { id: ids },
+            })
+            .toString();
+
+        return [apiBaseUrl, url].join('');
     }
 }
