197bd2ec8e85cf9d1b9d0ae24634a9d97f1ea680
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-02-26 11:08:34 +0100

    Add example form

--- conditional-form-validation/src/app/app.component.ts
+++ conditional-form-validation/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'conditional-form-validation';
+  constructor(private formBuilder: FormBuilder) {}
+
+  public emailForm = this.formBuilder.group({
+      isActive: this.formBuilder.control(false),
+      recipient: this.formBuilder.control('', Validators.required),
+      subject: this.formBuilder.control(''),
+      message: this.formBuilder.control(''),
+    }, {
+      updateOn: "submit"
+  });
+
+  public onSubmit(): void {
+    // Mark complete form as touched to enforce form validation
+    this.emailForm.markAsTouched();
+
+    console.log(this.emailForm.valid);
+  }
 }
