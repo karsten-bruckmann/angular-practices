5793e035de2e66c75a4cd79c70547e9cbd7e4e6d
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-02-26 11:12:25 +0100

    Add lint and fix errors

--- conditional-form-validation/src/app/app.component.ts
+++ conditional-form-validation/src/app/app.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder) {}
 
-  public emailForm = this.formBuilder.group({
-      isActive: this.formBuilder.control(false),
-      recipient: this.formBuilder.control('', Validators.required),
-      subject: this.formBuilder.control(''),
-      message: this.formBuilder.control(''),
-    }, {
-      updateOn: "submit"
-  });
+    public emailForm = this.formBuilder.group(
+        {
+            isActive: this.formBuilder.control(false),
+            recipient: this.formBuilder.control('', Validators.required),
+            subject: this.formBuilder.control(''),
+            message: this.formBuilder.control(''),
+        },
+        {
+            updateOn: 'submit',
+        }
+    );
 
-  public onSubmit(): void {
-    // Mark complete form as touched to enforce form validation
-    this.emailForm.markAsTouched();
+    public onSubmit(): void {
+        // Mark complete form as touched to enforce form validation
+        this.emailForm.markAsTouched();
 
-    console.log(this.emailForm.valid);
-  }
+        console.log(this.emailForm.valid);
+    }
 }
