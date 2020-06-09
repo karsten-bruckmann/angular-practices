e154fe7f9bd1a3b12280bbc7964380905da7449f
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-06-09 16:48:59 +0200

    Add example form

--- conditional-form-validation/src/app/app.component.ts
+++ conditional-form-validation/src/app/app.component.ts
@@ -1,30 +1,102 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms';
+
+export type BooleanFn = () => Boolean;
+
+export function conditionalValidator(
+    condition: BooleanFn,
+    validators: ValidatorFn[]
+): ValidatorFn {
+    return (formControl: FormControl): ValidationErrors | null => {
+        if (formControl.parent === undefined) {
+            return null;
+        }
+
+        if (condition() === false) {
+            return null;
+        }
+
+        // Iterate through validators and return first error
+        for (const validator of validators) {
+            const error = validator(formControl);
+
+            if (error !== null) {
+                return error;
+            }
+        }
+
+        return null;
+    };
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     constructor(private formBuilder: FormBuilder) {}
 
-    public emailForm = this.formBuilder.group(
-        {
-            isActive: this.formBuilder.control(false),
-            recipient: this.formBuilder.control('', Validators.required),
-            subject: this.formBuilder.control(''),
-            message: this.formBuilder.control(''),
-        },
-        {
-            updateOn: 'submit',
-        }
-    );
+    public form: FormGroup;
+
+    public ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            email: this.formBuilder.group({
+                isActive: [false],
+                recipient: [
+                    '',
+                    conditionalValidator(
+                        () => this.form.get('email.isActive').value,
+                        [Validators.required]
+                    ),
+                ],
+                subject: [
+                    '',
+                    conditionalValidator(
+                        () => this.form.get('email.isActive').value,
+                        [Validators.required]
+                    ),
+                ],
+                message: [
+                    '',
+                    conditionalValidator(
+                        () => this.form.get('email.isActive').value,
+                        [Validators.required]
+                    ),
+                ],
+            }),
+        });
+    }
 
     public onSubmit(): void {
-        // Mark complete form as touched to enforce form validation
-        this.emailForm.markAsTouched();
+        this.updateValueAndValidityOfAllControls(this.form);
 
-        console.log(this.emailForm.valid);
+        console.log(this.form.get('email').valid, this.form.status);
+    }
+
+    private updateValueAndValidityOfAllControls(formGroup: FormGroup): void {
+        const keys = Object.keys(formGroup.controls);
+        for (const key of keys) {
+            const control = formGroup.get(key);
+
+            if (control instanceof FormGroup) {
+                control.updateValueAndValidity({
+                    onlySelf: true,
+                    emitEvent: false,
+                });
+                this.updateValueAndValidityOfAllControls(control);
+            }
+
+            control.updateValueAndValidity({
+                emitEvent: false,
+            });
+        }
     }
 }
